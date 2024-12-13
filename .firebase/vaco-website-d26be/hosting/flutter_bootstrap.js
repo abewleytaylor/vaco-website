b92d9dfd52ad81d699d68886fe767b7186ab4090
Alex Bewley-Taylor <a.bewleytaylor@gmail.com>
2024-12-13 08:42:09 +0000

    Add firebase deployment generated files

--- .firebase/vaco-website-d26be/hosting/flutter_bootstrap.js
+++ .firebase/vaco-website-d26be/hosting/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2366739936"
+    serviceWorkerVersion: "2863468316"
   }
 });
